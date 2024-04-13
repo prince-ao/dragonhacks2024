10d0bf3dd9e401a9341815c9cce6cca206110447
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-04-13 08:17:46 -0400

    templates

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("index.j2")
 
 if __name__ == '__main__':
     app.run(debug=True)
