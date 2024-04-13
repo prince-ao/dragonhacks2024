f4246ce168d81b6948db581a45976e7501d58648
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-04-13 12:24:18 -0400

    done

--- audio_handle.py
+++ audio_handle.py
@@ -1,9 +1,14 @@
 from flask import request
 from flask_restx import Namespace, Resource
+from scripts.nlp import convert_audio_to_text
 
 audio_handle_ns = Namespace('handle-audio', description='audio handling module')
 
 @audio_handle_ns.route('/')
 class AudioHandler(Resource):
     def post(self):
-        print(request.get_data())
+        body = request.get_json()
+
+
+        print(body['data'])
+        print(convert_audio_to_text(body['data']))
---
+++ scripts/nlp.py
@@ -0,0 +1,36 @@
+import base64
+import io
+from pydub import AudioSegment
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+def convert_ogg_to_wav(ogg_data):
+    audio = AudioSegment.from_file(io.BytesIO(ogg_data), format="ogg")
+    wav_io = io.BytesIO()
+    audio.export(wav_io, format="wav")
+    wav_io.seek(0)
+    return wav_io
+
+def process_audio(data):
+    text = ""
+    try:
+        wav_audio = convert_ogg_to_wav(data)
+
+        with sr.AudioFile(wav_audio) as source:
+            audio = r.record(source)
+
+            try:
+                text = r.recognize_google(audio)
+            except Exception as e:
+                print(e)
+    except Exception as e:
+        print(e)
+
+    return text
+
+def convert_audio_to_text(base64_audio):
+    binary_data = base64.b64decode(base64_audio)
+    result = process_audio(binary_data)
+
+    return result
