61e450275de27581aa0816cc03046ec1b6ee3b83
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-04-13 08:49:42 -0400

    merging

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from taipy import Gui
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/record')
 def hello_world():
     return render_template("index.j2")
 
+gui = Gui(page="# Taipy application", flask=app)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    gui.run()
