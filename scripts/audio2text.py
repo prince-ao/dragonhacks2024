377dfef8a4ed81934035eae7aac2cc4194bdca55
kshitij <kshitij.k.research@gmail.com>
2024-04-13 12:50:12 -0400

    Created method to convert audio2text

---
+++ scripts/audio2text.py
@@ -0,0 +1,13 @@
+import whisper
+        
+
+def convert_audio_to_text(model, filepath):
+    result = model.transcribe(filepath, temperature=0., fp16=False)
+    return result['text']
+
+
+
+if __name__ == '__main__':
+    model = whisper.load_model("medium")
+    text = convert_audio_to_text(model, 'harvard.wav')
+    print(text)
